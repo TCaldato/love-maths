13569762cc88d3c0e8a45b77fa67adc350876258
TCaldato <tcaldato@gmail.com>
2023-07-14 15:31:53 +0100

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
 
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer () {
+   
+    
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+
+}
+
